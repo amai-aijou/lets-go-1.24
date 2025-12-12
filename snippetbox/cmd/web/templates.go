eabc8eef8f981be0acb6b4dac5c0316c9f555d75
amai-aijou <joseph.russ.olson@gmail.com>
2025-12-11 19:26:26 -0600

    5.6

--- snippetbox/cmd/web/templates.go
+++ snippetbox/cmd/web/templates.go
@@ -3,6 +3,7 @@ package main
 import (
 	"html/template" // 5.3
 	"path/filepath" // 5.3
+	"time" //5.6
 
 	"snippetbox.nerv.com/internal/models"
 )
@@ -16,8 +17,20 @@ type templateData struct {
 	Snippets	[]models.Snippet // 5.2
 }
 
+// 5.5: Create a humanDate function which returns a nicely-formatted string representation of time.Time value
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+// 5.5: initialize a template.FuncMap value and store in a global variable. basically a string-keyed map that
+// acts as a lookup table, mapping names to functions
+var functions = template.FuncMap{
+	"humanDate": humanDate,
+}
+
 // 5.3: Function to parse templates at application start, then store as in-memory cache
 func newTemplateCache() (map[string]*template.Template, error) {
+
 	// 5.3: Initialize a new map to as memory cache
 	cache := map[string]*template.Template{}
 
@@ -29,11 +42,13 @@ func newTemplateCache() (map[string]*template.Template, error) {
 
 	// 5.3: Loop through the page filepaths one-by-one
 	for _, page := range pages {
+
 		// 5.3: Extract filename (like 'home.tmpl') from the full filepath and assign to name variable
 		name := filepath.Base(page)
 
-		// 5.3: Parse the base template file into a template set
-		ts, err := template.ParseFiles("./ui/html/base.tmpl")
+		//5.5 template.FuncMap must be registered with the template set before calling ParseFiles().
+		// We have to use template.New() to create empty template set, use Funcs() to register template.FuncMap, then parse
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl")
 		if err != nil {
 			return nil, err
 		}
