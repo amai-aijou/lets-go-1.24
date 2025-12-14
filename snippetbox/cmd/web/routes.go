6e64ff5500c1141e27c74eddb0938670b9ff35df
amai-aijou <joseph.russ.olson@gmail.com>
2025-12-14 16:48:19 -0600

    6.5

--- snippetbox/cmd/web/routes.go
+++ snippetbox/cmd/web/routes.go
@@ -1,6 +1,11 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+
+	"github.com/justinas/alice" // 6.5: middleware management package	
+)
 
 // 6.2: Wrap servemux with commonHeaders middleware to add Headers to each request.
 // Now returns http.Handler instead of *http.ServeMux
@@ -18,8 +23,14 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
 	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
+	// 6.5: Create middleware chain containing 'standard' middleware, used for every request application receives
+	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
+
 	// 6.2: Passes the servemux as "next" parameter to commonHeaders middleware, which later returns http.Handler
 	// 6.3: Wrap the 6.2 part in logRequest(); now logRequest -> commonHeaders -> servemux -> application handler
 	// 6.4: WRAP IT AGAIN BOYS!!! this time, to recover from panics
-	return app.recoverPanic(app.logRequest(commonHeaders(mux)))
+	//return app.recoverPanic(app.logRequest(commonHeaders(mux)))
+
+	// 6.5: Instead of returning the multiple wraps, it will return the 'standard' middlewar chain followed by servemux
+	return standard.Then(mux)
 }
